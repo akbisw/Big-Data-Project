3a040359f7ff1b397b497be2c04195fbb9f99492
Amit Biswas <ab4309@nyu.edu>
2018-04-15 17:21:27 -0400

    setup_hdfs script to download datasets and setup hdfs

--- setup_hdfs.sh
+++ setup_hdfs.sh
@@ -1,7 +1,12 @@
 # Download Datasets
 cd data
-wget https://ndownloader.figshare.com/files/4975923 --no-check-certificate -O weather
-wget https://ndownloader.figshare.com/files/4944970 --no-check-certificate -O citibike
+if [ ! -f weather ]; then
+    wget https://ndownloader.figshare.com/files/4975923 --no-check-certificate -O weather;
+fi
+if [ ! -f citibike ]; then
+    wget https://ndownloader.figshare.com/files/4944970 --no-check-certificate -O citibike;
+fi
 # Prepare HDFS
 sh load-hdfs-structure
 sh load-spatial
+sh load-datasets
