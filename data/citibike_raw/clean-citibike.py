df1f68c61acc8c356f8d0f890e4fe09f9c1ce950
Amit Biswas <ab4309@nyu.edu>
2018-04-30 18:05:23 -0400

    Updated clean-citibike.py to make certain columns irrelevant for analysis in data polygamy framework.

--- data/citibike_raw/clean-citibike.py
+++ data/citibike_raw/clean-citibike.py
@@ -16,29 +16,31 @@ def try_parsing_date(text):
             pass
     raise ValueError('no valid date format found for this date: ' + text)
 
-def map_date_to_epoch(x):
-    # Retrieve the start and end times from list x
-    local_start_time = x[1].strip()
-    local_end_time = x[2].strip()
+def map_date_to_epoch(local_time):
     # define timezones from pytz library
     utc = pytz.utc
     eastern = pytz.timezone('US/Eastern')
     # Create a datetime object for each time
-    date_start = try_parsing_date(local_start_time)
-    date_end = try_parsing_date(local_end_time)
+    date = try_parsing_date(local_time)
     # Normalize (daylight) and localize (eastern time zone) the datetime objects
-    date_start_est = eastern.normalize(eastern.localize(date_start,is_dst=True))
-    date_end_est = eastern.normalize(eastern.localize(date_end,is_dst=True))
+    date_est = eastern.normalize(eastern.localize(date,is_dst=True))
     # Convert to UTC
-    date_start_utc = date_start_est.astimezone(utc)
-    date_end_utc = date_end_est.astimezone(utc)
+    date_utc = date_est.astimezone(utc)
     # Convert to epoch
-    ts_start = (date_start_utc - datetime(1970, 1, 1, tzinfo=utc)).total_seconds()
-    ts_end = (date_end_utc - datetime(1970, 1, 1, tzinfo=utc)).total_seconds()
-    # Write the epoch back into the list
-    x[1] = int(ts_start)
-    x[2] = int(ts_end)
+    ts = (date_utc - datetime(1970, 1, 1, tzinfo=utc)).total_seconds()
+    return int(ts)
+
+def mapper(x):
+    # Convert start and end dates to epoch
+    x[1] = map_date_to_epoch(x[1])
+    x[2] = map_date_to_epoch(x[2])
+    # Add $$ around columns not required
+    x[4] = '${}$'.format(x[4])
+    x[8] = '${}$'.format(x[8])
+    x[12] = '${}$'.format(x[12])
+    x[13] = '${}$'.format(x[13])
+    x[14] = '${}$'.format(x[14])
     # return the list as a csv string
     return ",".join(str(y) for y in x) 
 
-citibikeRdd = citibikeRdd.map(map_date_to_epoch).saveAsTextFile("data/citibike2013to2017")
+citibikeRdd = citibikeRdd.map(mapper).saveAsTextFile("data/citibike2013to2017")
